5ca8b7630f7dd69892ab85768d31feedbff1111d
PekiDroiid <luiscp_51@hotmail.com>
2024-05-30 11:07:09 -0500

    Cambios en entities

--- Domain/Entities/Tarea.cs
+++ Domain/Entities/Tarea.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +10,14 @@ namespace Domain.Entities
 {
     public class Tarea
     {
+        [Key]
         public int Id { get; set; }
         public string? Nombre { get; set; }
         public string? Descripcion { get; set; }
         public string? Estatus { get; set; }
-        public int FkAdmin { get; set; }
-        public int FkCamarista { get; set; }
-        public Usuario? Admin { get; set; }
-        public Usuario? Camarista { get; set; }
+
+        [ForeignKey("Usuario")]
+        public int FkUsuario { get; set; }
+        public Usuario? Usuario { get; set; }
     }
 }
--- RoomClean/Context/ApplicationDBContext.cs
+++ RoomClean/Context/ApplicationDBContext.cs
@@ -1,6 +1,10 @@
-﻿namespace RoomClean.Context
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RoomClean.Context
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
     }
 }
